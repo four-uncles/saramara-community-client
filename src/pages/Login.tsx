d528c6cc7f80db2a66a5328b61cbab1169a1a2ae
moon <xmun74@naver.com>
2024-02-21 22:43:16 +0900

    style: Login, SignUp Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,37 @@
+import { ROUTE_PATH } from '@/constants/path';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <div>로그인 페이지</div>;
+  const [errMsg, setErrMsg] = useState('');
+
+  const handleLogin = () => {
+    setErrMsg('에러 테스트');
+  };
+
+  return (
+    <main className="flex justify-center">
+      <div className="w-[400px]">
+        <div className="text-accent-300 text-2xl font-bold flex justify-center font-gmarket py-4 mb-10">로그인</div>
+        <div className="font-bold text-sm">아이디</div>
+        <input className="w-full input-default" placeholder="아이디를 입력해주세요." />
+        <div className="input-default-err-msg">{errMsg}</div>
+        <div className="font-bold text-sm mt-2">비밀번호</div>
+        <input className="w-full input-default" placeholder="비밀번호를 입력해주세요." />
+        <div className="input-default-err-msg">{errMsg}</div>
+        <button className="w-full px-4 py-2 bg-accent-300 text-white rounded my-10 font-bold" onClick={handleLogin}>
+          로그인
+        </button>
+
+        <div className="flex justify-between">
+          <span className="text-slate-400">아직 회원이 아니신가요?</span>
+          <Link to={ROUTE_PATH.SIGNUP} className="text-accent-300">
+            회원가입
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,43 @@
+import { ROUTE_PATH } from '@/constants/path';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const SignUp = () => {
-  return <div>회원가입 페이지</div>;
+  const [errMsg, setErrMsg] = useState('');
+
+  const handleSignUp = () => {
+    setErrMsg('에러 테스트');
+  };
+
+  return (
+    <main className="flex justify-center">
+      <div className="w-[400px]">
+        <div className="text-accent-300 text-2xl font-bold flex justify-center font-gmarket py-4 mb-10">회원가입</div>
+        <div className="font-bold text-sm">아이디</div>
+        <input className="w-full input-default" placeholder="아이디를 입력해주세요." />
+        <div className="input-default-err-msg">{errMsg}</div>
+        <div className="font-bold text-sm mt-2">닉네임</div>
+        <input className="w-full input-default" placeholder="닉네임을 입력해주세요." />
+        <div className="input-default-err-msg">{errMsg}</div>
+        <div className="font-bold text-sm mt-2">비밀번호</div>
+        <input className="w-full input-default" placeholder="비밀번호를 입력해주세요." />
+        <div className="input-default-err-msg">{errMsg}</div>
+        <div className="font-bold text-sm mt-2">비밀번호 확인</div>
+        <input className="w-full input-default" placeholder="재확인할 비밀번호를 입력해주세요." />
+        <div className="input-default-err-msg">{errMsg}</div>
+        <button className="w-full px-4 py-2 bg-accent-300 text-white rounded my-10 font-bold" onClick={handleSignUp}>
+          회원가입
+        </button>
+
+        <div className="flex justify-between">
+          <span className="text-slate-400">이미 아이디가 있으신가요?</span>
+          <Link to={ROUTE_PATH.LOGIN} className="text-accent-300">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default SignUp;
