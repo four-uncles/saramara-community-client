e709bf7d06afc90252bdd1fdb386f691e59f42f5
moon <xmun74@naver.com>
2024-04-10 22:29:29 +0900

    feat: react-hook-form 설치, SignUp api 세팅완료

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,11 +1,19 @@
 import { axiosInstance } from '.';
 
 /** MEMBER */
-export const signUp = async (signData: { memberId: string; nickname: string; password: string }) => {
-  const { data } = await axiosInstance.post(`/api/v1/member/signup`, signData);
+export const signUp = async (signData: { email: string; password: string; nickname: string }) => {
+  const { data } = await axiosInstance.post(`/api/v1/member`, signData);
   return data;
 };
-export const login = async (loginData: { loginEmail: string; loginPassword: string }) => {
+export const login = async (loginData: { email: string; password: string }) => {
   const { data } = await axiosInstance.post(`/api/v1/member/login`, loginData);
+  return data;
+};
+export const getMyInfo = async (memberId: number) => {
+  const { data } = await axiosInstance.get(`/api/v1/member/${memberId}`);
+  return data;
+};
+export const logout = async () => {
+  const { data } = await axiosInstance.get(`/api/v1/member/logout`);
   return data;
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,44 +1,108 @@
-// import { signUp } from '@/api/member';
-import { ROUTE_PATH } from '@/constants/path';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { signUp } from '@/api/member';
+import { ROUTE_PATH } from '@/constants/path';
+
+type SignUpInputType = {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirm: string;
+};
 
 const SignUp = () => {
-  // const [memberId, setMemberId] = useState('');
-  // const [nickname, setNickname] = useState('');
-  // const [password, setPassword] = useState('');
-  const [errMsg, setErrMsg] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<SignUpInputType>();
 
-  const handleSignUp = async () => {
-    setErrMsg('에러 테스트');
-    // const signPayload = {
-    //   memberId,
-    //   nickname,
-    //   password,
-    // };
-    // const res = await signUp(signPayload);
-    // console.log('회원가입 :', res);
+  const onValidPwd = (data: SignUpInputType) => {
+    if (data.password !== data.passwordConfirm) {
+      setError('passwordConfirm', { message: '비밀번호가 다릅니다' }, { shouldFocus: true });
+    }
+  };
+  const onSubmit: SubmitHandler<SignUpInputType> = async data => {
+    const { email, nickname, password } = data;
+    onValidPwd(data);
+    const signData = { email, nickname, password };
+    console.log('전송할 데이터', signData);
+    const res = await signUp(signData);
+    console.log('응답 결과', res);
   };
 
   return (
     <main className="flex justify-center">
       <div className="w-[400px]">
         <div className="text-accent-300 text-2xl font-bold flex justify-center font-gmarket py-4 mb-10">회원가입</div>
-        <div className="font-bold text-sm">아이디</div>
-        <input className="w-full input-default" placeholder="아이디를 입력해주세요." />
-        <div className="input-default-err-msg">{errMsg}</div>
-        <div className="font-bold text-sm mt-2">닉네임</div>
-        <input className="w-full input-default" placeholder="닉네임을 입력해주세요." />
-        <div className="input-default-err-msg">{errMsg}</div>
-        <div className="font-bold text-sm mt-2">비밀번호</div>
-        <input className="w-full input-default" placeholder="비밀번호를 입력해주세요." />
-        <div className="input-default-err-msg">{errMsg}</div>
-        <div className="font-bold text-sm mt-2">비밀번호 확인</div>
-        <input className="w-full input-default" placeholder="재확인할 비밀번호를 입력해주세요." />
-        <div className="input-default-err-msg">{errMsg}</div>
-        <button className="w-full px-4 py-2 bg-accent-300 text-white rounded my-10 font-bold" onClick={handleSignUp}>
-          회원가입
-        </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="font-bold text-sm">이메일</div>
+          <input
+            className="w-full input-default"
+            placeholder="이메일를 입력해주세요."
+            type="email"
+            {...register('email', {
+              required: true,
+              pattern: {
+                value: /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/,
+                message: '이메일 형식을 맞춰주세요 예) test@email.com',
+              },
+            })}
+          />
+          {errors.email && !errors.email.message && (
+            <span className="text-xs text-red-500 pl-2">이메일을 입력해주세요</span>
+          )}
+          {errors.email?.message && <span className="text-xs text-red-500 pl-2">{errors.email.message}</span>}
+
+          <div className="font-bold text-sm mt-2">닉네임</div>
+          <input
+            className="w-full input-default"
+            placeholder="닉네임을 입력해주세요."
+            {...register('nickname', {
+              required: true,
+              minLength: {
+                value: 3,
+                message: '닉네임을 3자 이상 입력해주세요',
+              },
+            })}
+          />
+          {errors.nickname && !errors.nickname?.message && (
+            <span className="text-xs text-red-500 pl-2">닉네임을 입력해주세요</span>
+          )}
+          {errors.nickname?.message && <span className="text-xs text-red-500 pl-2">{errors.nickname.message}</span>}
+
+          <div className="font-bold text-sm mt-2">비밀번호</div>
+          <input
+            className="w-full input-default"
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            {...register('password', { required: true })}
+          />
+          {errors.password && <span className="text-xs text-red-500 pl-2">비밀번호를 입력해주세요</span>}
+
+          <div className="font-bold text-sm mt-2">비밀번호 확인</div>
+          <input
+            className="w-full input-default"
+            placeholder="재확인할 비밀번호를 입력해주세요."
+            type="password"
+            {...register('passwordConfirm', { required: true })}
+          />
+          {errors.passwordConfirm && !errors.passwordConfirm?.message && (
+            <span className="text-xs text-red-500 pl-2">확인용 비밀번호를 입력해주세요</span>
+          )}
+          {errors.passwordConfirm?.message && (
+            <span className="text-xs text-red-500 pl-2">{errors.passwordConfirm.message}</span>
+          )}
+
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-accent-300 text-white rounded my-10 font-bold"
+            onClick={handleSubmit(onSubmit)}
+          >
+            회원가입
+          </button>
+        </form>
 
         <div className="flex justify-between">
           <span className="text-slate-400">이미 아이디가 있으신가요?</span>
